e367d84df20801e1f7d4e4da62e064e2337439e5
Peter J. Mello <admin@petermello.net>
2020-01-28 06:26:59 -0800

    Make meson proj. version dynamic from d/changelog
    Signed-off-by: Peter J. Mello <admin@petermello.net>

--- HardcodeTray/tools.py
+++ HardcodeTray/tools.py
@@ -35,7 +35,7 @@ def detect_de(de_list):
         if desktop in de_list:
             Logger.debug("DE: {0}".format(desktop.title()))
             return desktop
-        elif desktop == "gnome-xorg":
+        if desktop == "gnome-xorg":
             Logger.debug(
                 "DE: {0} (falling back to gnome)".format(desktop.title())
                 )
