f25233efda3c84f0d8de1687eaa7888b306d3159
Peter J. Mello <admin@petermello.net>
2020-03-28 05:09:50 -0700

    Add KVIrc support and refine VeraCrypt entries
    Signed-off-by: Peter J. Mello <admin@petermello.net>

--- HardcodeTray/enum.py
+++ HardcodeTray/enum.py
@@ -1,106 +1,82 @@
+#!/usr/bin/python3
+"""Fixes Hardcoded tray icons in Linux.
+
+Project: Hardcode-Tray <https://github.com/bil-elmoussaoui/Hardcode-Tray>
+SPDX-License-Identifier: GPL-3.0-or-later
+
+Copyright (C) 2017 Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program. If not, see <https://www.gnu.org/licenses/>.
 """
-Fixes Hardcoded tray icons in Linux.
-
-Author : Bilal Elmoussaoui (bil.elmoussaoui@gmail.com)
-Contributors : Andreas Angerer, Joshua Fogg
-Website : https://github.com/bil-elmoussaoui/Hardcode-Tray
-Licence : The script is released under GPL, uses a modified script
-     form Chromium project released under BSD license
-This file is part of Hardcode-Tray.
-Hardcode-Tray is free software: you can redistribute it and/or
-modify it under the terms of the GNU General Public License as published
-by the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-Hardcode-Tray is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-You should have received a copy of the GNU General Public License
-along with Hardcode-Tray. If not, see <http://www.gnu.org/licenses/>.
-"""
-from importlib import import_module
-from os import path
-
-from HardcodeTray.const import ARCH, USERHOME
-from HardcodeTray.utils import get_exact_folder
-
-from HardcodeTray.modules.log import Logger
 
 
-class Path:
-    """
-        Path class:
-        Check if paths do exists
-    """
+class Action:
+    """Apply (forcefully) or revert icon changes."""
+    APPLY = 1
+    REVERT = 2
+    CLEAR_CACHE = 3
 
-    DB_VARIABLES = {
-        "{userhome}": USERHOME,
-        "{size}": 22,
-        "{arch}": ARCH,
-        "{dropbox}": "dropbox_callback",
-        "{hangouts}": "hangouts_callback"
-    }
+    @staticmethod
+    def choices():
+        """Return a dict of different choices."""
+        choices = {}
+        for choice in Action.__dict__:
+            if hasattr(Action, choice):
+                try:
+                    value = int(getattr(Action, choice))
+                    choices[value] = choice
+                except (TypeError, ValueError):
+                    pass
+        return choices
 
-    def __init__(self, absolute_path, parser, path_key):
-        self._path = absolute_path
-        self._parser = parser
-        self.type = path_key
-        self._exists = True
-        self._validate()
 
-    def __add__(self, filename):
-        return self.path + filename
+class ConversionTools:
+    """Provide the various conversion backends."""
+    INKSCAPE = "Inkscape"
+    CAIROSVG = "CairoSVG"
+    RSVGCONVERT = "RSVGConvert"
+    SVGEXPORT = "SVGExport"
+    IMAGEMAGICK = "ImageMagick"
 
-    def __radd__(self, filename):
-        return filename + self.path
+    @staticmethod
+    def choices():
+        """Return a dict of different choices."""
+        choices = {}
+        for choice in ConversionTools.__dict__:
+            if hasattr(ConversionTools, choice):
+                value = getattr(ConversionTools, choice)
+                if (isinstance(value, str) and
+                        choice not in ["__module__", "__doc__"]):
+                    choices[value] = choice
+        return choices
 
-    def __str__(self):
-        return self.path
 
-    @property
-    def parser(self):
-        """Return Parser instance."""
-        return self._parser
+class ApplicationType:
+    """Define the types of supported applications."""
+    ELECTRON = "ElectronApplication"
+    ZIP = "ZipApplication"
+    PAK = "PakApplication"
+    NWJS = "NWJSApplication"
+    QT = "QtApplication"
+    APPLICATION = "Application"
 
-    @property
-    def path(self):
-        """Return the path."""
-        return self._path
-
-    @path.setter
-    def path(self, value):
-        self._path = value
-
-    @property
-    def exists(self):
-        """Return wether the path exists or not."""
-        return self._exists
-
-    def _validate(self):
-        """
-            Check wether a folder path exists or not.
-        """
-        from HardcodeTray.app import App
-
-        Path.DB_VARIABLES["{size}"] = str(App.icon_size())
-
-        for key, value in Path.DB_VARIABLES.items():
-            if key in self.path:
-                if value.endswith("_callback"):  # Check wether it's a function or not
-                    self._validate_with_callback(key, value)
-                else:
-                    self.path = self.path.replace(key, str(value))
-
-        if self.parser.script and self.type == "icons_path":
-            binary_file = path.join(self.path, self.parser.binary)
-            self._exists = path.exists(self.path) and path.exists(binary_file)
-        else:
-            self._exists = path.exists(self.path)
-
-    def _validate_with_callback(self, key, callback):
-        module = import_module("HardcodeTray.path")
-        if hasattr(module, callback):
-            method = getattr(module, callback)
-            self.path = get_exact_folder(key, self.path, method)
-            Logger.debug("Path with condition: "
-                         "{} {}".format(callback, self.path))
+    @staticmethod
+    def choices():
+        """Return a dict of different choices."""
+        choices = {}
+        for choice in ApplicationType.__dict__:
+            if hasattr(ApplicationType, choice):
+                value = getattr(ApplicationType, choice)
+                if isinstance(value, str) and choice != "__module__":
+                    choices[choice.lower()] = value
+        return choices
